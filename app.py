ebae556a7e7b9d3988aeeb520b38b0c8d3a67c6b
Liagson <aliaga.alberto@gmail.com>
2015-09-16 16:54:28 +0200

    procesado historial

--- app.py
+++ app.py
@@ -32,32 +32,38 @@ def apertura_fichero():
 	
 	return fichero_timeline
 
-def historial(api, usuario):
+def procesado_historial(api, usuario):
 	"""
 	Primero se lee la id del ultimo tweet que fue registrado en el historial
 	La id de este tweet sera devuelta por esta funcion para hacer una busqueda de tweets posteriores
 	Se sobreescribe por el ultimo tweet leido en el momento de ejecutar este programa
 	"""
 	
-	fichero_timeline = apertura_fichero()	
-	linea_fichero_historial = fichero_timeline.readline().split()
 	
+	usuario_coincide = False
+
+	while not usuario_coincide:
+		linea_fichero_historial = fichero_timeline.readline().split()
+		if (linea_fichero_historial0] == usuario) or (len(linea_fichero_historial) < 2):
+			usuario_coincide = True
+
 	public_tweets = api.user_timeline(id = usuario, count = 1)
 	ultimo_tweet = public_tweets[0].id
-	fichero_timeline.write(str(ultimo_tweet))
-	
-	fichero_timeline.close()			
 
-	if (ultimo_tweet_leido == 0): 
-		return ultimo_tweet
-	else: 
-		return ultimo_tweet_leido
+	if (len(linea_fichero_historial) < 2):
+		ultimo_tweet_leido = ultimo_tweet
+	else:
+		ultimo_tweet_leido = int(linea_fichero_historial[1])
+
+	return ultimo_tweet_leido, linea_fichero_historial
 
 def listado_tweets(api, list_usuarios):
+	fichero_timeline = apertura_fichero()
+
 	for id_usuario in list_usuarios:
 		print "Tweets de", id_usuario, "sin leer:",
 		
-		id_historial = historial(api, id_usuario)
+		id_historial = procesado_historial(api, id_usuario, fichero_timeline)
 		if (id_historial != -1):
 			public_tweets = api.user_timeline(id = id_usuario, count = 1, since_id = id_historial)
 			print len(public_tweets)
